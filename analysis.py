1c512b815a6311fa463d8aa5823b4b053d74d580
sam3llis <samwilliamellis@gmail.com>
2022-03-10 20:44:01 +0000

    feat: add analysis.py for quickly previewing data

---
+++ analysis.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+F_DIR = os.path.join(os.getcwd(), 'data', 'UAD')
+
+
+def plot_data(fpath):
+    df = pd.read_csv(fpath, parse_dates=['date'], dayfirst=True)
+    print(df.info())
+    plt.plot(df.date, df.price)
+    plt.title(os.path.split(fpath)[1].split('.')[0].replace('_', ' '))
+    plt.xlabel('Date')
+    plt.ylabel('Price, £')
+    plt.xticks(rotation=45)
+    plt.show()
+
+
+def main():
+    fpath = os.path.join(F_DIR, 'api_500_series_eq_collection.csv')
+    plot_data(fpath)
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
